64be1d1ea3e6291702faa78b8a87a99ee2dbdb61
SofiVBastos <sofia.basto@sptech.school>
2023-03-11 00:22:30 -0300

    alteração do link do hamburguer

--- site/public/js/scripit.js
+++ site/public/js/scripit.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "/site/public/assets/imgs/menu_white_36dp.svg";
+        document.querySelector('.icon').src = "../public/assets/imgs/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "/site/public/assets/imgs/close_white_36dp.svg";
+        document.querySelector('.icon').src = "../public/assets/imgs/close_white_36dp.svg";
     }
 }
