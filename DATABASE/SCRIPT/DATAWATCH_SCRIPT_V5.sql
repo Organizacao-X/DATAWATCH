a8c966a00a4fe3eec5713fc0be32f30494be7927
LucasSouza1407 <lucasmilagres@sptech.school>
2023-04-11 20:36:07 -0300

    Alterações temporaria BD

--- DATABASE/SCRIPT/DATAWATCH_SCRIPT_V5.sql
+++ DATABASE/SCRIPT/DATAWATCH_SCRIPT_V5.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE IF NOT EXISTS datawatch;
 USE datawatch;
 
+
+
 CREATE TABLE IF NOT EXISTS Empresas (
 idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
 razaoSocial VARCHAR(45) NOT NULL,
@@ -50,9 +52,9 @@ discoMemoria varchar(45),
 ip VARCHAR(45),
 PRIMARY KEY (idMaquina, fkEmpresa),
 statusSistema TINYINT(1),
-cpuFrequencia DOUBLE,
-ramTotal DOUBLE,
-discoTotal DOUBLE
+cpuFrequencia BIGINT,
+ramTotal BIGINT,
+discoTotal BIGINT
 );
 
 
@@ -74,13 +76,16 @@ PRIMARY KEY (idCaptura, fkMaquina, fkEmpresa)
 );
 
 INSERT INTO Usuarios VALUES
-(1, 'lele', 'lele@gmail.com', 12312312312, '12345678', 1, 'IMAGEM', null, 1);
+(1, 'lele', 'lele@gmail.com', 12312312312, '12345678', 1, 'IMAGEM', null, null);
 INSERT INTO Empresas VALUES
 (1, 'novaempresa', 12312312312312, 12345123, 'Rua ali', '234', 'casa', 'Jd ali', 'Sao paulo', 'SP', 1);
+
+update Usuarios set fkEmpresa = 1 where idusuario = 1;
 INSERT INTO Contato VALUES
 (1, 'novae@gmail.com', '11 989898989');
-INSERT INTO Maquinas VALUES
-(1, 1, 'maquina01', 'GFT56', '2023-03-03', null, 'rh38tgh9327g', 1, 3.9, 8.0, 700.3);
+INSERT INTO Maquinas (idMaquina, fkEmpresa, nomeMaquina, serie, dtChegada, processador, ram, discoMemoria, ip, statusSistema, cpuFrequencia, ramTotal, discoTotal) 
+VALUES
+(1, 1, 'maquina01', 'GFT56', '2023-03-03', 'I5', '16 GB', 'HD 1 TB', '192.08.92.12', 1,2400000, 1600000, 100000000);
 INSERT INTO Capturas VALUES 
 (1, 1, 1, '2023-04-04 12:00:00', 3.4, 56.88, 4.5, 10.0, 25.5, 580.4);  
 
