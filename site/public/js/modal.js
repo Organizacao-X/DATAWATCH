7d939eb223b3d2a8baf575032ad0367b936e0c23
Sofia Bastos <sofia.bastos@sptech.school>
2023-04-20 11:21:15 -0300

    add modais

--- site/public/js/modal.js
+++ site/public/js/modal.js
@@ -1,96 +1,159 @@
-let $ = document
+let $ = document;
+// modais
+const modalEditarFuncionario = $.querySelector('.modal-parent1');
+const modalDesativarFuncionario = $.querySelector('.modal-parent2');
+const modalMostrarMais = $.querySelector('.modal-parent3');
+const modalEditarDateReiniciar = $.querySelector('.modal-parent4');
+const modalReiniciar = $.querySelector('.modal-parent5');
 
-const botaoEditar = $.querySelector('.abrir-editar')
-const botaoExcluir = $.querySelector('.abrir-excluir')
+// sessão
+const sessaoElem = $.querySelector('section');
 
+// Maquina lista
+const buttonMostrarMais = $.querySelector('.mais');
 
-const modallParent1 = $.querySelector('.modal-parent1')
-const modallParent2 = $.querySelector('.modal-parent2')
+const x3 = $.querySelector('.X-3');
 
-const x = $.querySelector('.X')
-const sair = $.querySelector('.exit')
-
-const cancelEdit = $.querySelector('#cancel_edit')
-const cancelDelete = $.querySelector('#cancel_delete')
-
-const saveEdit = $.querySelector('#save_edit')
-const saveDelete = $.querySelector('#save_delete')
-
-const sectionElem1 = $.querySelector('.parte1')
-const sectionElem2 = $.querySelector('.parte2')
-
-const textoAlertaSave = $.querySelector('.texto-alerta')
-
-function showModalEditar() {
-    modallParent1.style.display = 'block'
-    sectionElem1.style.filter = 'blur(5px)'
-    sectionElem2.style.filter = 'blur(5px)'
-    botaoEditar.blur()
+function abrirModalMostrarMais() {
+    modalMostrarMais.style.display = 'block'
+    sessaoElem.style.filter = 'blur(10px)'
+    buttonMostrarMais.blur()
 }
 
-function showModalExcluir(){
-    modallParent2.style.display = 'block'
-    sectionElem1.style.filter = 'blur(5px)'
-    sectionElem2.style.filter = 'blur(5px)'
-    botaoExcluir.blur()
+function fecharModalMostrarMaisComX() {
+    modalMostrarMais.style.display = 'none'
+    sessaoElem.style.filter = 'blur(0px)'
 }
 
-
-
-function hideModalWithCancelEditar() {
-    modallParent1.style.display = 'none'
-    sectionElem1.style.filter = 'blur(0px)'
-    sectionElem2.style.filter = 'blur(0px)'
-    textoAlertaSave.style.display = 'none'
-}
-
-function hideModalWithSaveEditar() {
-    sectionElem1.style.filter = 'blur(0px)'
-    sectionElem2.style.filter = 'blur(0px)'
-    textoAlertaSave.style.display = 'block'
-}
-
-function hideModalWithCancelExcluir(){
-    modallParent2.style.display = 'none'
-    sectionElem1.style.filter = 'blur(0px)'
-    sectionElem2.style.filter = 'blur(0px)'
-}
-
-function hideModalWithSaveExcluir() {
-    modallParent2.style.display = 'none'
-    sectionElem1.style.filter = 'blur(0px)'
-    sectionElem2.style.filter = 'blur(0px)'
-}
-
-
-
-function hideModalWithEscEditar(event) {
+function fecharModalMostrarMaisComEsc(event) {
     if (event.keyCode === 27) {
-        modallParent1.style.display = 'none'
-        sectionElem1.style.filter = 'blur(0px)'
-        sectionElem2.style.filter = 'blur(0px)'
-
+        modalMostrarMais.style.display = 'none'
+        sessaoElem.style.filter = 'blur(0px)'
     }
 }
 
-function hideModalWithEscExcluir(event) {
+buttonMostrarMais.addEventListener('click', abrirModalMostrarMais)
+x3.addEventListener('click', fecharModalMostrarMaisComX)
+document.body.addEventListener('keyup', fecharModalMostrarMaisComEsc)
+
+// funcionarios
+const buttonEditFuncionario = $.querySelector('.abrir-editar');
+const buttonDesativarFuncionario = $.querySelector('.abrir-desativar');
+
+const buttonSalvarEdit = $.querySelector('#save_edit');
+const buttonCancelarEdit = $.querySelector('#cancel_edit');
+
+const buttonConfirmDesativar = $.querySelector('#save_deactivate')
+const buttonCancelarDesativar = $.querySelector('#cancel_deactivate')
+
+const x1 = $.querySelector('.X-1');
+const x2 = $.querySelector('.X-2');
+
+function abrirModalEditarFuncionario() {
+    modalEditarFuncionario.style.display = 'block'
+    sessaoElem.style.filter = 'blur(10px)'
+    buttonEditFuncionario.blur()
+}
+
+function fecharModalEditarFuncionarioComX() {
+    modalEditarFuncionario.style.display = 'none'
+    sessaoElem.style.filter = 'blur(0px)'
+}
+
+function fecharModalEditarFuncionarioComEsc(event) {
     if (event.keyCode === 27) {
-        modallParent2.style.display = 'none'
-        sectionElem1.style.filter = 'blur(0px)'
-        sectionElem2.style.filter = 'blur(0px)'
+        modalEditarFuncionario.style.display = 'none'
+        sessaoElem.style.filter = 'blur(0px)'
     }
 }
 
+buttonEditFuncionario.addEventListener('click', abrirModalEditarFuncionario)
+x1.addEventListener('click', fecharModalEditarFuncionarioComX)
+document.body.addEventListener('keyup', fecharModalEditarFuncionarioComEsc)
 
-botaoEditar.addEventListener('click', showModalEditar)
-x.addEventListener('click', hideModalWithCancelEditar)
-cancelEdit.addEventListener('click', hideModalWithCancelEditar)
-saveEdit.addEventListener('click',hideModalWithSaveEditar)
-document.body.addEventListener('keyup', hideModalWithEscEditar)
+buttonSalvarEdit.addEventListener('click', fecharModalEditarFuncionarioComX)
+buttonCancelarEdit.addEventListener('click', fecharModalEditarFuncionarioComX)
 
-botaoExcluir.addEventListener('click', showModalExcluir)
-sair.addEventListener('click', hideModalWithCancelExcluir)
-cancelDelete.addEventListener('click', hideModalWithCancelExcluir)
-saveDelete.addEventListener('click',hideModalWithSaveExcluir)
-document.body.addEventListener('keyup', hideModalWithEscExcluir)
 
+function abrirModalDesativarFuncionario() {
+    modalDesativarFuncionario.style.display = 'block'
+    sessaoElem.style.filter = 'blur(10px)'
+    buttonDesativarFuncionario.blur()
+}
+
+function fecharModalDesativarFuncionarioComX() {
+    modalDesativarFuncionario.style.display = 'none'
+    sessaoElem.style.filter = 'blur(0px)'
+}
+
+function fecharModalDesativarFuncionarioComEsc(event) {
+    if (event.keyCode === 27) {
+        modalDesativarFuncionario.style.display = 'none'
+        sessaoElem.style.filter = 'blur(0px)'
+    }
+}
+
+buttonDesativarFuncionario.addEventListener('click', abrirModalDesativarFuncionario)
+x2.addEventListener('click', fecharModalDesativarFuncionarioComX)
+document.body.addEventListener('keyup', fecharModalDesativarFuncionarioComEsc)
+
+buttonConfirmDesativar.addEventListener('click', fecharModalDesativarFuncionarioComX)
+buttonCancelarDesativar.addEventListener('click', fecharModalDesativarFuncionarioComX)
+
+
+// execução maquinas
+const buttonEditarDateReiniciar = $.querySelector('.edit-date');
+const buttonReiniciar = $.querySelector('.reload');
+
+const buttonConfirmReiniciar = $.querySelector('#confirm_reiniciar')
+const buttonCancelarReiniciar = $.querySelector('#cancel_reiniciar')
+
+const x4 = $.querySelector('.X-4');
+const x5 = $.querySelector('.X-5');
+
+function abrirModalEditarDateReiniciar() {
+    modalEditarDateReiniciar.style.display = 'block'
+    sessaoElem.style.filter = 'blur(10px)'
+    buttonEditarDateReiniciar.blur()
+}
+
+function fecharModalEditarDateReiniciarComX() {
+    modalEditarDateReiniciar.style.display = 'none'
+    sessaoElem.style.filter = 'blur(0px)'
+}
+
+function fecharModalEditarDateReiniciarComEsc(event) {
+    if (event.keyCode === 27) {
+        modalEditarDateReiniciar.style.display = 'none'
+        sessaoElem.style.filter = 'blur(0px)'
+    }
+}
+
+buttonEditarDateReiniciar.addEventListener('click', abrirModalEditarDateReiniciar)
+x4.addEventListener('click', fecharModalEditarDateReiniciarComX)
+document.body.addEventListener('keyup', fecharModalEditarDateReiniciarComEsc)
+
+function abrirModalReiniciar() {
+    modalReiniciar.style.display = 'block'
+    sessaoElem.style.filter = 'blur(10px)'
+    buttonReiniciar.blur()
+}
+
+function fecharModalReiniciarComX() {
+    modalReiniciar.style.display = 'none'
+    sessaoElem.style.filter = 'blur(0px)'
+}
+
+function fecharModalReiniciarComEsc(event) {
+    if (event.keyCode === 27) {
+        modalReiniciar.style.display = 'none'
+        sessaoElem.style.filter = 'blur(0px)'
+    }
+}
+
+buttonReiniciar.addEventListener('click', abrirModalReiniciar)
+x5.addEventListener('click', fecharModalReiniciarComX)
+document.body.addEventListener('keyup', fecharModalReiniciarComEsc)
+
+buttonConfirmReiniciar.addEventListener('click', fecharModalReiniciarComX)
+buttonCancelarReiniciar.addEventListener('click', fecharModalReiniciarComX)
