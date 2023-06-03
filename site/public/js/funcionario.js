36825a8e166ea956c3ca3519686e8e193b0b83e8
NataSFSilva <nata.ssilva@sptech.school>
2023-06-03 20:16:33 -0300

    Botão de reiniciar na área de funcionário funcionando :D

--- site/public/js/funcionario.js
+++ site/public/js/funcionario.js
@@ -1,3 +1,27 @@
 function reiniciar() {
-    
+    var msg = document.getElementsByClassName("msgConfirm");
+
+    fetch(`/datawatch/rebootar`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            idMaquinaServer: sessionStorage.ID_MAQUINA,
+            fkEmpresaServer: sessionStorage.ID_EMPRESA
+        })
+    }).then(function (resposta) {
+
+        if (resposta.ok) {
+            // window.alert("Máquina reiniciada com sucesso!");
+            msg[0].style.color = "#000";
+            msg[0].style.animation = "fade-in 1s ease-in-out";
+        } else if (resposta.status == 404) {
+            window.alert("Deu 404!");
+        } else {
+            throw ("Houve um erro ao tentar rebootar a máquina: " + resposta.status);
+        }
+    }).catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+    });
 }
